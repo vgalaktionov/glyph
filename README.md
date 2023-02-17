1e0b11d4080fd5a40aaf1f27443b3e48d3998141
Vadim Galaktionov <vadim.galaktionov@source.ag>
2023-02-17 20:34:53 +0100

    feat: console renderer

---
+++ README.md
@@ -0,0 +1,17 @@
+# Glyph
+
+> Roguelike in Typescript
+
+## Development
+
+First time:
+
+```bash
+npm install
+```
+
+Then:
+
+```bash
+npm run dev
+```
---
+++ src/console.ts
@@ -0,0 +1,6 @@
+import * as ROT from 'rot-js'
+
+let display = new ROT.Display({ width: 40, height: 9, layout: "term" });
+display.draw(5, 4, "@", null, null);
+display.draw(15, 4, "%", "#0f0", null);          // foreground color
+display.draw(25, 4, "#", "#f00", "#009");  // and background color
