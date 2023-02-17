0e6b13e21f8aeb20d2278c73c5b902c9ca5863b7
Vadim Galaktionov <vadim.galaktionov@source.ag>
2023-02-17 21:22:26 +0100

    style: prettier

--- src/console.ts
+++ src/console.ts
@@ -1,6 +1,7 @@
-import * as ROT from 'rot-js'
+import * as ROT from "rot-js";
 
-let display = new ROT.Display({ width: 40, height: 9, layout: "term" });
+const display = new ROT.Display({ width: 40, height: 9, layout: "term" });
+
 display.draw(5, 4, "@", null, null);
-display.draw(15, 4, "%", "#0f0", null);          // foreground color
-display.draw(25, 4, "#", "#f00", "#009");  // and background color
+display.draw(15, 4, "%", "#0f0", null); // foreground color
+display.draw(25, 4, "#", "#f00", "#009"); // and background color
